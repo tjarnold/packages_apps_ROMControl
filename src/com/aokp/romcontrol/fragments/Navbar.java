73817eb71d325102ff90ca235de73165f0af0510
Stevespear426 <stephen.k.spear@gmail.com>
2013-04-22 16:51:50 +0200

    RC: NavBar fix some selection being off and refresh buttons
    Change-Id: Ibfdc3c1f36f2f1ab59b154524ed477b1b6f4830a

--- src/com/aokp/romcontrol/fragments/Navbar.java
+++ src/com/aokp/romcontrol/fragments/Navbar.java
@@ -300,6 +300,7 @@ public class Navbar extends AOKPPreferenceFragment implements
                         Settings.System.NAVIGATION_CUSTOM_APP_ICONS[1], "");
                 Settings.System.putString(mContentRes,
                         Settings.System.NAVIGATION_CUSTOM_APP_ICONS[2], "");
+                loadButtons();
                 refreshSettings();
                 return true;
             default:
@@ -658,9 +659,9 @@ public class Navbar extends AOKPPreferenceFragment implements
                 mPicker.pickShortcut();
         } else { // This should be any other defined action.
             if (button.getPickLongPress()) {
-                button.setLongPress(AwesomeConstants.AwesomeActions()[command]);
+                button.setLongPress(mActionCodes[command]);
             } else {
-                button.setClickAction(AwesomeConstants.AwesomeActions()[command]);
+                button.setClickAction(mActionCodes[command]);
             }
         }
         refreshButtons();
